3fa4d272cd7b1e711ea7aa1aa345bc2083d90709
Dylan Ambrose <djambro44@gmail.com>
2020-05-05 16:00:10 -0700

    added file structure and created DB

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Royale', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom & Swiss', false);
