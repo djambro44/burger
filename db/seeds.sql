247f97e20dfd2505e41120edc40ac70ed1eaa81c
Dylan Ambrose <djambro44@gmail.com>
2020-05-07 15:37:56 -0700

    added orm.js content

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name, devoured) VALUES ('cheeseburger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Royale', true);
-INSERT INTO burgers (name, devoured) VALUES ('Mushroom & Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom & Swiss', false);
